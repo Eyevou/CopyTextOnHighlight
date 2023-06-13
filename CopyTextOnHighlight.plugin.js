e466f9421f4f37d7dfefdcd61fb3b920f3eb0ea1
Eyevou <40645381+Eyevou@users.noreply.github.com>
2023-06-13 19:45:18 -0400

    Update CopyTextOnHighlight.plugin.js
    Handles turning off now.

--- CopyTextOnHighlight.plugin.js
+++ CopyTextOnHighlight.plugin.js
@@ -2,15 +2,14 @@
  * @name CopyTextOnHighlight
  * @author Eyevou
  * @description Copies any highlighted text to the user's clipboard.
- * @version 1.0.1
+ * @version 1.0.2
  */
 
 module.exports = class CopyTextOnHighlight {
-	start() {
-		window.addEventListener('mouseup', function() {if ( document.getSelection().toString() != '') {document.execCommand('copy');}},false);
-	} 
+	start() { window.addEventListener('mouseup', CopyEventHandler)} 
+	stop()  { window.removeEventListener('mouseup', CopyEventHandler)}
+}
 
-	stop() {
-		// Nothing to clean.
-	}
+var CopyEventHandler = function() {
+	if ( document.getSelection().toString() != '') {document.execCommand('copy');}
 }
